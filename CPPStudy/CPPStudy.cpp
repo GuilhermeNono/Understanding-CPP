9e83bd1538006ae77f6d1ee0f6e2561fcf163faa
Guilherme Nono <guilherme.nono45789@gmail.com>
2024-09-03 10:59:14 -0300

    feat: Adicionando arquivos a section 8

--- CPPStudy/CPPStudy.cpp
+++ CPPStudy/CPPStudy.cpp
@@ -6,20 +6,13 @@ int main()
 
     setlocale(LC_ALL, "portuguese");
     int numero = 234;
-    int *ptr = &numero;
+    int &Ref = numero;
 
-    *ptr = 1456;
-
-    std::cout << "\nO valor de numero é: " << *ptr;
+    std::cout << "\nO valor de Ref é: " << Ref;
+    std::cout << "\nO valor de Numero é: " << numero;
+    std::cout << "\nO Endereço de Ref é: " << &Ref;
     std::cout << "\nO Endereço de numero é: " << &numero;
-    std::cout << "\nO Endereço do ponteiro é: " << &ptr;
 
-    int *outroPtr = ptr;
-
-    *outroPtr = *ptr + 200;
-
-    std::cout << "\n O valor de numero é: " << *outroPtr;
-    
     return 0;
 }
 
