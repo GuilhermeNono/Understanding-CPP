73a27d79a3525cd92e5804d6a58eef7f71c93bf8
Guilherme Nono <guilherme.nono45789@gmail.com>
2024-09-03 09:00:39 -0300

    feat: Finalizando section 6

--- CPPStudy/CPPStudy.cpp
+++ CPPStudy/CPPStudy.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <vector>
+
+#include "solution.h"
 
 int main()
 {
-    system("PAUSE");
+    solutions sol;
+
+    std::vector<int> params = {2,7,11,15};
+
+    const std::vector<int> result = sol.two_sum(params, 18);
+
+    std::cout << "[" << result[0] << ", " << result[1] << "]" << '\n';
+    
     return 0;
 }
 
---
+++ CPPStudy/solution.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+#include <vector>
+
+class solutions{
+public:
+    std::vector<int> two_sum(std::vector<int>& nums, int target);
+};
